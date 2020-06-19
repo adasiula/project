5856083364d854b15fb79e94435252a063a7228a
adasiula <65869257+adasiula@users.noreply.github.com>
2020-06-19 23:03:02 +0200

    add files

---
+++ bullet.h
@@ -0,0 +1,34 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include <enemy.h>
+
+
+class Bullet:public sf::Sprite,sf::RectangleShape
+{
+public:
+    sf::CircleShape shape;
+    sf::Vector2f currVelocity;
+    float maxspeed;
+    int speedshoot;
+
+    Bullet(float radius=3.0);
+    void shooting(sf::RenderWindow &w,std::vector<Bullet> &b,sf::Vector2f p,const std::vector<sf::Sprite> &walls,std::vector<sf::Sprite> &enemies);
+    //void shooting2(sf::RenderWindow &w,std::vector<Bullet> &b,sf::Vector2f p,const std::vector<sf::Sprite> &walls,std::vector<Enemy> &en);
+
+    void shooting3(sf::RenderWindow &w,std::vector<Bullet> &b,sf::Vector2f p,const std::vector<sf::Sprite> &walls,std::vector<Enemy> &en);
+
+    void settx(sf::Texture &tx);
+private:
+    sf::Vector2f playercenter;
+    sf::Vector2f mousepos;
+    sf::Vector2f aimdir;
+    sf::Vector2f aimdirnorm;
+    int sp=0;
+};
+
+#endif // BULLET_H
